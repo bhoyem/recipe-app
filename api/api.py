e2b90d55df3ca5ad1f2fd53f437e8a26a135d70b
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-09-07 13:25:01 -0500

    Added routes for post, put, and delete to add, modify, and delete recipes in the database

--- api/api.py
+++ api/api.py
@@ -1,4 +1,3 @@
-import time
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -14,6 +13,7 @@ class Recipe(db.Model):
     description = db.Column(db.Text, nullable=True, default='Delicious. You need to try it!')
     image_url = db.Column(db.String(500), nullable=True, default="https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
     servings = db.Column(db.Integer, nullable=False)
+    
     def __repr__(self):
         return f"Recipe(id={self.id}, title='{self.title}', description='{self.description}', servings={self.servings})"
     
@@ -42,35 +42,80 @@ def get_all_recipes():
 @app.route('/api/recipes', methods=['POST'])
 def add_recipe():
     data = request.get_json()
-    
+    #Validate the incoming data to ensure it has the required fields
     required_fields = ['title', 'ingredients', 'instructions', 'servings', 'description', 'image_url']
+
     for field in required_fields:
-            if field not in data or data[field] == "":
-                return jsonify({'error': f"Missing required field: '{field}'"}), 400
+        if field not in data or data[field] == "":
+            return jsonify({'error': f"Missing required field: '{field}'"}), 400
     
     new_recipe = Recipe(
         title=data['title'],
         ingredients=data['ingredients'],
         instructions=data['instructions'],
+        servings=data['servings'],
         description=data['description'],
-        image_url=data['image_url'],
-        servings=data['servings']
+        image_url=data['image_url']
     )
+
     db.session.add(new_recipe)
-    db.session.commmit()
+    db.session.commit()
 
     new_recipe_data = {
         'id': new_recipe.id,
         'title': new_recipe.title,
         'ingredients': new_recipe.ingredients,
         'instructions': new_recipe.instructions,
-        'description': new_recipe.description,
-        'image_url': new_recipe.image_url,
         'servings': new_recipe.servings,
+        'description': new_recipe.description,
+        'image_url': new_recipe.image_url
     }
 
     return jsonify({'message': 'Recipe added successfully', 'recipe': new_recipe_data})
 
+@app.route('/api/recipes/<int:recipe_id>', methods=['PUT'])
+def update_recipe(recipe_id):
+    recipe = Recipe.query.get(recipe_id)
+    if not recipe:
+        return jsonify({'error': 'Recipe not found'}), 404
+    
+    data = request.get_json()
+
+    #Validate the incoming json data to ensure it has the requried fields
+    required_fields = ['title', 'ingredients', 'instructions', 'servings', 'description', 'image_url']
+
+    for field in required_fields:
+        if field not in data or data[field] == "":
+            return jsonify({'error': f"Missing required field: '{field}'"}), 400
+            
+    recipe.title = data['title']
+    recipe.ingredients = data['ingredients']
+    recipe.instructions = data['instructions']
+    recipe.servings = data['servings']
+    recipe.description = data['description']
+    recipe.image_url = data['image_url']
+    db.session.commit()
+    
+    updated_recipe = {
+        'id': recipe.id,
+        'title': recipe.title,
+        'ingredients': recipe.ingredients,
+        'instructions': recipe.instructions,
+        'servings': recipe.servings,
+        'description': recipe.description,
+        'image_url': recipe.image_url
+    }
+
+    return jsonify({'message': 'Recipe updated successfully', 'recipe': updated_recipe})
+
+@app.route('/api/recipes/<int:recipe_id>', methods=['DELETE'])
+def delete_recipe(recipe_id):
+    recipe = Recipe.query.get(recipe_id)
+    if not recipe:
+        return jsonify({'error': 'Recipe not found'}), 404
+    db.session.delete(recipe)
+    db.session.commit()
+    return jsonify({'message': 'Recipe deleted successfully'})
 
 
 if __name__ == '__main__':
